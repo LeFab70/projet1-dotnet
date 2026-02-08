b870780f5de48b4c43c6c47ebfbda44c783945ba
LeFab70 <kouonang2002@gmail.com>
2026-02-08 04:27:59 -0400

    init

---
+++ VentesForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Projet1
+{
+    partial class VentesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listCommadClientSpecButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listVenteButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listCommadClientSpecButton
+            // 
+            this.listCommadClientSpecButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listCommadClientSpecButton.Location = new System.Drawing.Point(20, 52);
+            this.listCommadClientSpecButton.Name = "listCommadClientSpecButton";
+            this.listCommadClientSpecButton.Size = new System.Drawing.Size(421, 38);
+            this.listCommadClientSpecButton.TabIndex = 0;
+            this.listCommadClientSpecButton.Text = "Liste des commandes d\'un client spécifique";
+            this.listCommadClientSpecButton.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listVenteButton);
+            this.panel1.Controls.Add(this.listCommadClientSpecButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(468, 193);
+            this.panel1.TabIndex = 1;
+            // 
+            // listVenteButton
+            // 
+            this.listVenteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listVenteButton.Location = new System.Drawing.Point(20, 113);
+            this.listVenteButton.Name = "listVenteButton";
+            this.listVenteButton.Size = new System.Drawing.Size(421, 38);
+            this.listVenteButton.TabIndex = 1;
+            this.listVenteButton.Text = "Liste des ventes par année";
+            this.listVenteButton.UseVisualStyleBackColor = true;
+            // 
+            // VentesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 193);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "VentesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ventes";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button listCommadClientSpecButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button listVenteButton;
+    }
+}
+
